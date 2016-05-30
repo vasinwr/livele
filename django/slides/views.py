38c84a2483491ed066a6f5f05b3e0ed11e8b1980
Vasin W <vasinwr@gmail.com>
2016-05-30 19:58:28 +0100

    template change to support websocket communication

--- django/slides/views.py
+++ django/slides/views.py
@@ -13,27 +13,27 @@ from django.contrib.auth import logout
 @login_required
 def index(request):
     if(request.user.groups.filter(name = 'Lecturer').count() == 1):
-	return render(request, 'slides/index.html', {'lecturer':True, 'lectureList': Slides.objects.values_list('slide_text', flat=True).distinct()})
+        return render(request, 'slides/index.html', {'lecturer':True, 'lectureList': Slides.objects.values_list('slide_text', flat=True).distinct()})
     else:
-	return render(request, 'slides/index.html', {'lectureList': Slides.objects.values_list('slide_text', flat=True).distinct()})
+        return render(request, 'slides/index.html', {'lectureList': Slides.objects.values_list('slide_text', flat=True).distinct()})
 
 @login_required
 def select(request, name):
 #deactive all other actives.
     try:
-	other = Current.objects.get(owner = request.user, active=1)
-	other.active = 0
-	other.save()
+        other = Current.objects.get(owner = request.user, active=1)
+        other.active = 0
+        other.save()
     except Current.DoesNotExist:
-	pass
+        pass
 
     try:
-	current = Current.objects.get(owner = request.user, slide_name = name)
-	current.active = 1
-	current.save()
+        current = Current.objects.get(owner = request.user, slide_name = name)
+        current.active = 1
+        current.save()
     except Current.DoesNotExist:
-	current = Current(owner = request.user, slide_name = name, page=1, active=1)
-	current.save()
+        current = Current(owner = request.user, slide_name = name, page=1, active=1)
+        current.save()
 
     return HttpResponseRedirect(reverse('slides:lecture'))
 
@@ -52,9 +52,9 @@ def lecture(request):
     return render(request, 'slides/index.html', {'slide': slide})
     '''
     try:
-	current_slide = Slides.objects.get(slide_text = current.slide_name, page= current.page)
+        current_slide = Slides.objects.get(slide_text = current.slide_name, page= current.page)
     except Slides.DoesNotExist:
-	return HttpResponseNotFound()
+        return HttpResponseNotFound()
     canVote = Current.objects.get(owner = current_slide.lecturer, slide_name = current_slide.slide_text).page >= current.page
     if(canVote and request.user.groups.filter(name = 'Student').count() == 1):
         try:
@@ -67,8 +67,8 @@ def lecture(request):
     bad = Votes.objects.filter(slide = current_slide, value = 1).count()
     total = good + bad
     if (total == 0):
-	total = 1
-	good = 1
+        total = 1
+        good = 1
     if(request.user.groups.filter(name = 'Lecturer').count() == 1):
         return render(request, 'slides/lecture.html', {'slide':current_slide, 'lecturer':True, 'votes_amplified':bad *100 / total, 'votes_rest': good*100/total})
     else:
@@ -101,9 +101,9 @@ def curr_page(request):
     mycurr = get_object_or_404(Current, owner = request.user, active=1)
     lecturer = Slides.objects.get(slide_text = mycurr.slide_name, page=1).lecturer
     try:
-	lecture_curr = Current.objects.get(slide_name = mycurr.slide_name, owner = lecturer)
-	mycurr.page = lecture_curr.page
-	mycurr.save()
+        lecture_curr = Current.objects.get(slide_name = mycurr.slide_name, owner = lecturer)
+        mycurr.page = lecture_curr.page
+        mycurr.save()
     except Slides.DoesNotExist:
         return HttpResponseNotFound()
     return HttpResponseRedirect(reverse('slides:lecture'))
@@ -120,15 +120,15 @@ def vote_up(request):
     current_slide = Slides.objects.get(slide_text = current.slide_name, page= current.page)
 
     if (Current.objects.get(owner = current_slide.lecturer, slide_name = current_slide.slide_text).page >= current.page):
-	try:
-	    v = Votes.objects.get(user = request.user, slide = current_slide)
-	    v.value = 0
-	    v.save()
-	except Votes.DoesNotExist:
-	    v = Votes(user = request.user, slide = current_slide, value = 0)
-	    v.save()
+        try:
+            v = Votes.objects.get(user = request.user, slide = current_slide)
+            v.value = 0
+            v.save()
+        except Votes.DoesNotExist:
+            v = Votes(user = request.user, slide = current_slide, value = 0)
+            v.save()
     else:
-	pass
+        pass
     return HttpResponseRedirect(reverse('slides:lecture'))
 
 @login_required
@@ -143,15 +143,15 @@ def vote_down(request):
     current_slide = Slides.objects.get(slide_text = current.slide_name, page= current.page)
 
     if (Current.objects.get(owner = current_slide.lecturer, slide_name = current_slide.slide_text).page >= current.page):
-	try:
-	    v = Votes.objects.get(user = request.user, slide = current_slide)
-	    v.value = 1
-	    v.save()
-	except Votes.DoesNotExist:
-	    v = Votes(user = request.user, slide = current_slide, value = 1)
-	    v.save()
+        try:
+            v = Votes.objects.get(user = request.user, slide = current_slide)
+            v.value = 1
+            v.save()
+        except Votes.DoesNotExist:
+            v = Votes(user = request.user, slide = current_slide, value = 1)
+            v.save()
     else:
-	pass
+        pass    
     return HttpResponseRedirect(reverse('slides:lecture'))
 
 '''
