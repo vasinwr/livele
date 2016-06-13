d2b478d03fccf204b76c38200d94d775e759e556
hcl114 <hcl114@ic.ac.uk>
2016-06-13 16:27:30 +0100

    fixed prev next

--- lecturer_view/app/scripts/controllers/lecture.js
+++ lecturer_view/app/scripts/controllers/lecture.js
@@ -13,7 +13,6 @@ app.controller('LectureCtrl', function($scope, $window, $location, $http){
   $scope.navi_hover  =false ;
   $scope.home_hover  =false ;
   $scope.close_hover =false ;
-  $scope.page_num = 1;
   $scope.ques = [{question: 'what happens if I fail?', votes: 10}, 
                {question: 'are labs open on bank holiday', votes: 8},
                {question: 'what happened to our coke machine', votes: 3}];
@@ -35,17 +34,9 @@ app.controller('LectureCtrl', function($scope, $window, $location, $http){
     ctrl.clicked = true;
   };
   $scope.prev = function(){
-
-    $http.get('http://127.0.0.1:8000/slides/lecture/go_prev_page/').success(function(data){
-
-     ctrl.page_num = data.page;
-    });
+    return $http.get('http://127.0.0.1:8000/slides/lecture/go_prev_page/')
   };
   $scope.next = function(){
-    console.log('next')
-    $http.get('http://127.0.0.1:8000/slides/lecture/go_next_page/').success(function(data){
-     console.log('done')
-     ctrl.page_num = data.page;
-    });
+    return $http.get('http://127.0.0.1:8000/slides/lecture/go_next_page/')
   };
 });
