963d39d4f116463086d31ef4c6179275e4e4a2af
Vasin W <vasinwr@gmail.com>
2016-06-13 21:50:49 +0100

    logout from clicker

--- lecturer_view/app/scripts/controllers/clicker.js
+++ lecturer_view/app/scripts/controllers/clicker.js
@@ -1,4 +1,25 @@
-app.controller('ClickerCtrl', function($scope, $window, $location, $http){
+app.controller('ClickerCtrl', function($scope, $window, $location, $http, AuthService){
+  if (!$window.localStorage.token) {
+    $location.path('/clicker_login');
+    return;
+  }
+  if ($window.localStorage.user_is_lec != 1) {
+    AuthService.logout().then(
+      function(){
+        $location.path('/clicker_login');
+      });
+    return;
+  }
+  $scope.logout = function () {
+    AuthService.logout().then(
+      function () {
+        $location.path('/');
+      },
+      function (error) {
+        $scope.error = error;
+      }
+    );
+  };
   $scope.next = function () {
     $http.get('http://127.0.0.1:8000/slides/clicker_next/');
   };
