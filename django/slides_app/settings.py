038bdccc7955d0a9e9f460c0e771811f0170137c
jiranart <jiranart.vacheesuthum12@imperial.ac.uk>
2016-06-14 23:06:16 +0100

    settings and wsgi

--- django/slides_app/settings.py
+++ django/slides_app/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+#added for heroku datb
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#added for heroku
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
-    'corsheaders'
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -51,7 +55,8 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    #added for heroku
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'slides_app.urls'
@@ -122,15 +127,40 @@ USE_L10N = True
 
 USE_TZ = True
 
+#----ADDED FOR HEROKU DATABASE COMPATIBILITY------
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+#------------------------END OF CHANGE-----------
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+#ADDED FOR HEROKU DATABASE COMPATIBILITY - static root / staticfiledirs
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
 
-
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#----ADDED FOR HEROKU DATABASE COMPATIBILITY------
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#------------------------END OF CHANGE-----------
 
 #for channels -nonblocking layer (websocket support etc)
 CHANNEL_LAYERS = {
@@ -140,9 +170,3 @@ CHANNEL_LAYERS = {
     }
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = (
-   # '127.0.0.1:9000',
-)
-
-CORS_ALLOW_CREDENTIALS = True
